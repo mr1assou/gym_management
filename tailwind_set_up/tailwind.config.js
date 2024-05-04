b2c9b23ea9c53391eab6e15a3fa51bda0d0d53d9
Marwane Assou <marwane.assou@gmail.com>
2024-05-04 22:57:49 +0100

    set up tailwind css

---
+++ tailwind_set_up/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors:{
+        'green':'#74f814',
+        'white':'#f3fcec',
+        'black':'#000000'
+      }
+    },
+  },
+  plugins: [],
+}
+
