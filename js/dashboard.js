017bede72da6e131928485182032df3f7c959cdb
Marwane Assou <marwane.assou@gmail.com>
2024-05-12 17:17:48 +0100

    fetch data and diplay it in dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,6 @@
 
 function clickLinksSideBar(){
     const links=document.querySelectorAll('.link-page');
-    console.log(links);
     links.forEach((link)=>{
     link.addEventListener('click',(e)=>{
             const sibling=e.currentTarget.children[1];
@@ -10,3 +9,14 @@ function clickLinksSideBar(){
     })
 }
 clickLinksSideBar();
+
+function statusColor(){
+    const status=document.querySelectorAll('.status');
+    console.log(status);
+    status.forEach(item=>{
+        if(item.textContent=='trial') item.classList.add('text-blue');
+        else if(item.textContent=='access') item.classList.add('text-green-dark');
+        else if(item.textContent=='reject') item.classList.add('text-red');
+    })
+}
+statusColor();
